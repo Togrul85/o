354d6dab6a2b2b19809da8347c29e12f8b02d0b0
unknown <toghrul@code.edu.az>
2022-08-07 23:09:43 +0400

    .

---
+++ bbbb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 7, 9, 10, 11, 12 };
+            int num = 8;
+            Console.WriteLine(Number(arr,num));
+        }
+        static bool Number (int [] arr,int num)
+        {
+            for (int i = 0; i <arr.Length; i++)
+            {
+                if (arr[i]== num)
+                {
+                    return true;
+                }
+
+
+            }
+            return false;
+        }
+    }
+}
